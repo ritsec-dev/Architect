3f7bb6c14b7cc84f8de75f81eda94a506ee0af58
Joshua Niemann <jspike397@users.noreply.github.com>
2019-10-02 11:59:13 -0400

    Added Dialog box for Adding Member.  TODO: align right, test with api endpoint

--- frontend/src/components/CreateMember.js
+++ frontend/src/components/CreateMember.js
@@ -6,84 +6,141 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';    
+import { withStyles } from '@material-ui/styles';
+import API from '../API';
 
 
-export default function CreateMember() {
-    const [open, setOpen] = React.useState(false);
-  
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
+const styles = theme => ({
+  nothing: {
+    color: "black",
+  }, 
+  CreateMemberButton: {
+    display: 'flex',
+    flex: 1,
+    marginLeft: 'auto',
+    position: 'relative',
+    textAlign: 'center',
+    justifyContent: 'flex-end',
+  },
+})
+
+
+
+class CreateMember extends React.Component
+{
+
+    handleOpen = () => {
+      this.setState({open: true});
+
     };
 
-    const sendData = () => { 
-        setOpen(false);
+    handleClose = () => {
+      this.setState({open: false});
+
     };
-  
-    return (
-      <div>
-        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-          Add Member
-        </Button>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">Add Member</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Please enter member information.
-            </DialogContentText>
-            
-            <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Name"
-                type="text"
-                fullWidth
+    onChange=(e)=> {
+      this.setState({ [e.target.id]: e.target.value});
+    }
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: '',
+        email: '',
+        phone: '',
+        role: '',
+        team: '',
+        open: false
+      }
+      this.onChange = this.onChange.bind(this);
+      this.handleOpen = this.handleOpen.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleSubmit = event => {
+      const { name, email, phone, role, team } = this.state;
+      API.post('/user/add', {"name": name, "email": email, "phone": phone, "role": role , "team":team});
+      this.setState({open: false});
+    }
+
+    render() {
+         
+        //const { classes } = this.props;
+      
+        return (
+          <div>
+            <div style={this.state.CreateMemberButton}>
+              <Button variant="outlined" color="primary" onClick={this.handleOpen}>
+                Add Member
+              </Button>
+            </div>
+            <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+              <DialogTitle id="form-dialog-title">Add Member</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Please enter member information.
+                </DialogContentText>
                 
-            />
-            <TextField
-              margin="dense"
-              id="email"
-              label="RIT Email Address"
-              type="email"
-              fullWidth
-            />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                    type="text"
+                    onChange={this.onChange}
+                    fullWidth
+                    
+                />
+                <TextField
+                  margin="dense"
+                  id="email"
+                  label="RIT Email Address"
+                  type="email"
+                  onChange={this.onChange}
+                  fullWidth
+                />
 
-            <TextField
-                margin="dense"
-                id="role"
-                label="Role"
-                type="text"
-                fullWidth
-            />
+                <TextField
+                    margin = "dense"
+                    id = "phone"
+                    label = "Phone Number"
+                    type="tel"
+                    onChange={this.onChange}
+                    fullWidth
+                />
 
-            <TextField
-                margin = "dense"
-                id = "phone"
-                label = "Phone Number"
-                type="tel"
-            />
-            <TextField
-                margin="dense"
-                id="team"
-                label="Team"
-                type="text"
-                fullWidth
-            />
 
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={sendData} color="primary">
-              Add User
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
+                <TextField
+                    margin="dense"
+                    id="role"
+                    label="Role"
+                    type="text"
+                    onChange={this.onChange}
+                    fullWidth
+                />
+
+                <TextField
+                    margin="dense"
+                    id="team"
+                    label="Team"
+                    type="text"
+                    onChange={this.onChange}
+                    fullWidth
+                />
+
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={this.handleSubmit} color="primary">
+                  Add User
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+        );
+    }
   }
+
+export default withStyles(styles)(CreateMember)
